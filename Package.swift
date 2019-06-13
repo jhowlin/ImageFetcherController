59265fdfbe5599d04bf51e00476648a4208b360b
Jason Howlin <jhowlin@me.com>
2019-06-13 05:04:48 -0400

    Add swift package manager support

---
+++ Package.swift
@@ -0,0 +1,18 @@
+
+import PackageDescription
+
+let package = Package(
+    name: "ImageFetcherController",
+    products: [
+        .library(
+            name: "ImageFetcherController",
+            targets: ["ImageFetcherController""]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "ImageFetcherController",
+            dependencies: [],
+			path:"ImageFetcherController/Classes")
+    ]
+)
